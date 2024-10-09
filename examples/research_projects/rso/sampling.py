f0dac82f94a9d9881be1e7e065788f16df0a6564
SimonCuCu <97490786+SimonCuCu@users.noreply.github.com>
2024-10-09 19:48:56 +0800

    Update and rename generate_from_sft.py to sampling.py

--- examples/research_projects/rso/sampling.py
+++ examples/research_projects/rso/sampling.py
@@ -1,20 +1,9 @@
-# coding=utf-8
-# Copyright 2023 The HuggingFace Inc. team. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from dataclasses import dataclass, field
 from typing import Optional
+
+import pandas as pd
+from tqdm import tqdm
+
 
 from accelerate import Accelerator
 from datasets import Dataset, load_dataset
@@ -25,43 +14,57 @@ from transformers import (
     DataCollatorForSeq2Seq,
     HfArgumentParser
 )
+import torch
+import os 
+import time 
 
 from trl.trainer.utils import generate
 
+#import deepspeed
+#from deepspeed.utils.timer import SynchronizedWallClockTimer
+
+# Initialize the timer
+#timers = SynchronizedWallClockTimer()
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["LOCAL_RANK"] = "0"
+os.environ["RANK"] = "0"
+os.environ["WORLD_SIZE"] = "1"
+os.environ["MASTER_ADDR"] = "localhost"
+os.environ["MASTER_PORT"] = "12355"
 
 @dataclass
 class ScriptArguments:
     # model parameters
-    model_name_or_path: Optional[str] = field(default=None, metadata={"help": "the model name"})
-    mixed_precision: Optional[str] = field(default="fp16", metadata={"help": "the model dtype"})
+    model_name_or_path: Optional[str] = field(default="lomahony/pythia-160m-helpful-sft", metadata={"help": "the SFT model name"})
     # data parameters
-    dataset_name: Optional[str] = field(default="Dahoas/full-hh-rlhf", metadata={"help": "the HF data path"})
+    dataset_name: Optional[str] = field(default="trl-internal-testing/hh-rlhf-helpful-base-trl-style", metadata={"help": "the HF data path"})
+    dataset_col: Optional[str] = field(default="prompt", metadata={"help": "the HF data path"})
     split: Optional[str] = field(default="train", metadata={"help": "the dataset split to use for generation"})
     batch_size: Optional[int] = field(default=8, metadata={"help": "the generation batch size"})
-    max_prompt_length: Optional[int] = field(default=512, metadata={"help": "the maximum prompt length"})
-    save_dataset_path: Optional[str] = field(default="sft_gen_dataset", metadata={"help": "the path for saving the generated dataset"})
+    max_prompt_length: Optional[int] = field(default=100, metadata={"help": "the maximum prompt length"})
+    subset_size: Optional[int] = field(default=500, metadata={"help": "the number of samples to use from the dataset for debugging"})
+    save_dataset_path: Optional[str] = field(default="sft_gen_dataset_v2_notVLLM", metadata={"help": "the path for saving the generated dataset"})
     # generation parameters
-    max_new_tokens: Optional[int] = field(
-        default=128, metadata={"help": "the maximum number of tokens generated per sample"}
-    )
+    max_new_tokens: Optional[int] = field(default=128, metadata={"help": "the maximum number of tokens generated per sample"})
     temperature: Optional[float] = field(default=1.0, metadata={"help": "the sampling temperature"})
-    top_p: Optional[float] = field(default=1.0, metadata={"help": "top_p sampling argument"})
-    top_k: Optional[float] = field(default=50, metadata={"help": "top_k sampling argument"})
+    top_p: Optional[float] = field(default=0.98, metadata={"help": "top_p sampling argument"})
+    top_k: Optional[int] = field(default=25, metadata={"help": "top_k sampling argument"})
     num_return_sequences: Optional[int] = field(default=64, metadata={"help": "the number of return sequences"})
     # instrumentation
     sanity_check: Optional[bool] = field(default=False)
+    
+
 
 
 if __name__ == "__main__":
     parser = HfArgumentParser(ScriptArguments)
     script_args = parser.parse_args_into_dataclasses()[0]
 
-    accelerator = Accelerator(
-        mixed_precision=script_args.mixed_precision
-    )
+    accelerator = Accelerator()
     
     # load sft policy
-    model = AutoModelForCausalLM.from_pretrained(script_args.model_name_or_path)
+    model = AutoModelForCausalLM.from_pretrained(script_args.model_name_or_path,torch_dtype=torch.bfloat16,trust_remote_code=True)
     
     tokenizer = AutoTokenizer.from_pretrained(script_args.model_name_or_path)
 
@@ -81,14 +84,17 @@ if __name__ == "__main__":
         "num_return_sequences": script_args.num_return_sequences,
     }
 
+    #start_time=time.time()
     # load and preprocess the dataset
     dataset = load_dataset(script_args.dataset_name)[script_args.split]
+    dataset = dataset.select(range(min(len(dataset), script_args.subset_size)))
+
 
     if script_args.sanity_check:
         dataset = dataset.select(range(min(len(dataset), 100)))
 
     def tokenize_fn(samples):
-        model_inputs = tokenizer(samples["prompt"])
+        model_inputs = tokenizer(samples[script_args.dataset_col])
 
         return {
             **model_inputs,
@@ -96,6 +102,7 @@ if __name__ == "__main__":
 
     dataset = dataset.map(tokenize_fn, batched=True, remove_columns=list(dataset.features))
     dataset = dataset.filter(lambda x: len(x["input_ids"])<script_args.max_prompt_length)
+    #load_time = time.time() - start_time
 
     data_collator = DataCollatorForSeq2Seq(tokenizer, max_length=script_args.max_prompt_length, pad_to_multiple_of=8)
 
@@ -103,10 +110,16 @@ if __name__ == "__main__":
 
     model, dataloader = accelerator.prepare(model, dataloader)
 
-    # generate responses from sft policy
-    prompts, responses = generate(model, dataloader, tokenizer, accelerator, **generation_kwargs)
     
+    start_time=time.time()
+
+    # generate responses from sft policy
+    prompts, responses = generate(model, dataloader, tokenizer, accelerator, seed=0, **generation_kwargs)
+    generate_time = time.time() - start_time
+
     generated_dataset = Dataset.from_dict({"prompt": prompts, "response": responses})
     
     # save the generated dataset
     generated_dataset.save_to_disk(script_args.save_dataset_path)
+    #print(f"Time taken to load and preprocess dataset: {load_time:.2f} seconds")
+    print(f"Time taken to generate responses (GPU): {generate_time:.6f} seconds")
